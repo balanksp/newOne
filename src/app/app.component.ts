f82443ee473f2591b5e063dbcc887f0c1aede773
balanksp <a.bala101@gmail.com>
2023-07-14 16:55:32 +0530

    new application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'singlePageApp';
+
+  Heading: String = 'App components(one way binding from component to DOM)';
+  
+  textValueDisplay: String = 'text is editable';
+
+  onSubmit(): void {
+    alert("DOM to component");
+  }
+  
+ngDoCheck(): void{
+  console.log(this.textValueDisplay); 
+}
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataPageComponent } from './data-page/data-page.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { ChildComponent } from './child/child.component';
+
+
+const routes: Routes = [
+  { path: 'first', component:FirstComponent},
+  { path: 'second', component:SecondComponent},
+  {path: 'third', component:ThirdComponent},
+  { path: '', component:FirstComponent}
+];
+
+const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataPageComponent,FirstComponent,SecondComponent,ThirdComponent, ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,routing
   ],
   providers: [],
   bootstrap: [AppComponent]
